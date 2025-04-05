12360de727bd5054083259d34e2183320cd772c6
rcutte <rcutte@student.42.fr>
2025-04-05 11:25:33 +0200

    build: update /api endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target:
-          "https://https://hackathon-ia-et-crise.fr/tousconcernes/rag-system:8090/api/app",
+          "https://https://hackathon-ia-et-crise.fr/tousconcernes/rag-system/api/app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
